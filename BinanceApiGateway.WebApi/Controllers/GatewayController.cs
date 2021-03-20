93d20e37efb01bf950f331afe87d8de7406696bc
yagizhanyakali <yagizhanyakali@gmail.com>
2021-03-20 23:37:38 +0300

    Add project files.

---
+++ BinanceApiGateway.WebApi/Controllers/GatewayController.cs
@@ -0,0 +1,32 @@
+﻿using BinanceApiGateway.WebApi.Libraries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BinanceApiGateway.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GatewayController : ControllerBase
+    {
+        IBinanceClient BinanceClient;
+
+        public GatewayController(IBinanceClient binanceClient)
+        {
+            BinanceClient = binanceClient;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<string> GetAsync(string currencyName)
+        {
+            string result = await BinanceClient.GetCurrencyAsync(currencyName);
+
+            return result;
+        }
+    }
+}
---
+++ BinanceApiGateway.WebApi/Libraries/BinanceClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BinanceApiGateway.WebApi.Libraries
+{
+    public class BinanceClient : IBinanceClient
+    {
+        public async Task<string> GetCurrencyAsync(string currencyName)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync($"https://api.binance.com/api/v3/ticker/price?symbol={currencyName}");
+            response.EnsureSuccessStatusCode();
+
+            string data = await response.Content.ReadAsStringAsync();
+
+            return data;
+        }
+    }
+}
